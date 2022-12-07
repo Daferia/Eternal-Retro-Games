3db4c0d9d28000c7d50d16aad0a82d8fcd9212b5
Fabio Faria <daferia@gmail.com>
2022-12-07 00:07:58 +0000

    Added list display and customised the layout and order in Admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import Product, Manufacturer
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'manufacturer',
+        'selling_price',
+        'image',
+    )
 
-admin.site.register(Product)
-admin.site.register(Manufacturer)
+    ordering = ('name',)
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
