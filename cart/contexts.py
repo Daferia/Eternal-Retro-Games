f3f42d08274089140dd78cbf97c792a85c32439c
Fabio Faria <daferia@gmail.com>
2022-12-12 21:58:43 +0000

    added for loop to access the cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,6 +11,16 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
 
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.selling_price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
         free_delivery_delta =  settings.FREE_DELIVERY_THRESHOLD - total
