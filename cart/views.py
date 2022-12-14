f5b4df97688370e3e48e14d7fbec8360db25d868
Fabio Faria <daferia@gmail.com>
2022-12-14 00:17:01 +0000

    added messages to view in add_to_cart

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,7 @@ def add_to_cart(request, item_id):
     ''' 
     Add a qty of the individual item to the cart
     '''
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -21,8 +21,12 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
 
     request.session['cart'] = cart
     print(request.session['cart'])
