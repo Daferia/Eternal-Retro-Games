18a2f509e849a8d9515cc1843b3d2f88f573fdb1
Fabio Faria <daferia@gmail.com>
2022-12-15 00:43:34 +0000

    added basic checkout view to test the checkout form in checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.conf import settings
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
+from cart.contexts import cart_contents
+
 
 # Create your views here.
+def checkout(request):
+
+    order_form = OrderForm()
+   
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
