ffcfab014504880bc7c222ccee093052d810f133
Fabio Faria <daferia@gmail.com>
2022-12-15 21:32:04 +0000

    added stripe variable to checkout in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -8,15 +8,22 @@ from .models import Order, OrderLineItem
 from products.models import Product
 from cart.contexts import cart_contents
 
+import stripe
+import json
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order_form = OrderForm()
-   
+    
+    order_form = OrderForm() # needs to be deleted
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        # 'client_secret': intent.client_secret,
+        'client_secret': 'testdata',
     }
 
     return render(request, template, context)
