476519bab9fff0d37c35aa9fbc71981fad4521af
Fabio Faria <daferia@gmail.com>
2022-12-18 21:45:15 +0000

    change year_of_release to allow null or blank values

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254, null=False, blank=False)
     platform = models.CharField(max_length=35, blank=False, null=False)
     manufacturer = models.ForeignKey('Manufacturer', null=True, blank=True, on_delete=models.SET_NULL)
-    year_of_release = models.IntegerField()
+    year_of_release = models.IntegerField(null=True, blank=True,)
     genre = models.CharField(max_length=35, blank=True, null=True)
     publisher = models.CharField(max_length=35, blank=True, null=True)
     user_review = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
